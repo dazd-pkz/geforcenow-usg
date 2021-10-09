33f066cac9d994e00f9ef36cea0327d7d6d66552
PKZ <87366355+Dazd-Pkz@users.noreply.github.com>
2021-10-09 22:26:02 +0200

    Add files via upload

---
+++ pkz-usg.bat
@@ -0,0 +1,80 @@
+@echo off
+mode con: cols=26 lines=4
+color 1F
+title GeforceNow USG by DazdPKZ
+echo GeforceNow USG by DazdPKZ
+echo.
+echo github.com/dazd-pkz ; uwu
+timeout 2 /nobreak > nul
+cls
+mode con: cols=52 lines=8
+color 02
+echo killing steam...
+taskkill /f /im steam.exe
+echo trying to start steam...
+IF exist "C:\msqrd" C:&cd "C:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe"
+IF exist "B:\msqrd" B:&cd "B:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe" 
+IF exist "D:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe" 
+IF exist "E:\msqrd" B:&cd "B:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe" 
+IF exist "F:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe" 
+timeout 1 /nobreak > nul
+echo downloading sharex...
+IF exist "E:\msqrd" E:&cd "E:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://install/400040
+IF exist "F:\msqrd" F:&cd "F:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://install/400040
+IF exist "G:\msqrd" G:&cd "G:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://install/400040
+IF exist "H:\msqrd" H:&cd "H:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://install/400040
+IF exist "D:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://install/400040
+echo trying to start sharex...
+IF exist "E:\msqrd" E:&cd "E:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "F:\msqrd" F:&cd "F:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "G:\msqrd" G:&cd "G:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "H:\msqrd" H:&cd "H:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "D:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+echo antikick ^+ file explorer
+start "" "C:\windows\syswow64\hh.exe" B:\ &start "" "C:\windows\syswow64\hh.exe" C:\
+echo looping...
+timeout 3 /nobreak > nul
+mode con: cols=16 lines=2
+cls
+color 04
+echo l
+timeout 1 /nobreak > nul 
+cls
+color 06
+echo lo
+timeout 1 /nobreak > nul
+cls
+color 0e
+echo loa
+timeout 1 /nobreak > nul
+cls
+color 02
+echo load
+timeout 1 /nobreak > nul
+cls
+color 09
+echo loadi
+timeout 1 /nobreak > nul
+cls
+color 05
+echo loadin
+timeout 1 /nobreak > nul
+cls
+color 0d
+echo loading
+timeout 1 /nobreak > nul
+cls
+color 0c
+echo loading!
+timeout 2 /nobreak > nul
+:r
+cls
+color 4f
+echo pkz is the king
+timeout 1 /nobreak > nul
+cls
+color f4
+echo pkz is the king
+timeout 1 /nobreak > nul
+goto :r
+pause > nul
